109297cdbd6ef28a02a9ed619ab1ddb054fd1233
Thomas Cokelaer <cokelaer@gmail.com>
2020-09-26 00:06:15 +0200

    add new pipeline

---
+++ README.rst
@@ -0,0 +1,75 @@
+This is is the **ribofinder** pipeline from the `Sequana <https://sequana.readthedocs.org>`_ project
+
+:Overview: Simple parallele workflow to detect and report ribosomal content
+:Input: FastQ files
+:Output: HTML reports
+:Status: draft
+:Citation: Cokelaer et al, (2017), ‘Sequana’: a Set of Snakemake NGS pipelines, Journal of Open Source Software, 2(16), 352, JOSS DOI doi:10.21105/joss.00352
+
+
+Installation
+~~~~~~~~~~~~
+
+You must install Sequana first::
+
+    pip install sequana
+
+Then, just install this package::
+
+    pip install sequana_ribofinder
+
+
+Usage
+~~~~~
+
+::
+
+    sequana_pipelines_ribofinder --help
+    sequana_pipelines_ribofinder --input-directory DATAPATH 
+
+This creates a directory with the pipeline and configuration file. You will then need 
+to execute the pipeline::
+
+    cd ribofinder
+    sh ribofinder.sh  # for a local run
+
+This launch a snakemake pipeline. If you are familiar with snakemake, you can 
+retrieve the pipeline itself and its configuration files and then execute the pipeline yourself with specific parameters::
+
+    snakemake -s ribofinder.rules -c config.yaml --cores 4 --stats stats.txt
+
+Or use `sequanix <https://sequana.readthedocs.io/en/master/sequanix.html>`_ interface.
+
+Requirements
+~~~~~~~~~~~~
+
+This pipelines requires the following executable(s):
+
+- bowtie1
+
+.. image:: https://raw.githubusercontent.com/sequana/sequana_ribofinder/master/sequana_pipelines/ribofinder/dag.png
+
+
+Details
+~~~~~~~~~
+
+This pipeline runs **ribofinder** in parallel on the input fastq files (paired or not). 
+A brief sequana summary report is also produced.
+
+
+Rules and configuration details
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Here is the `latest documented configuration file <https://raw.githubusercontent.com/sequana/sequana_ribofinder/master/sequana_pipelines/ribofinder/config.yaml>`_
+to be used with the pipeline. Each rule used in the pipeline may have a section in the configuration file. 
+
+Changelog
+~~~~~~~~~
+
+========= ====================================================================
+Version   Description
+========= ====================================================================
+0.0.1     **First release.**
+========= ====================================================================
+
+
---
+++ sequana_pipelines/ribofinder/main.py
@@ -0,0 +1,127 @@
+import sys
+import os
+import argparse
+import shutil
+
+from sequana_pipetools.options import *
+from sequana_pipetools.misc import Colors
+from sequana_pipetools.info import sequana_epilog, sequana_prolog
+
+col = Colors()
+
+NAME = "ribofinder"
+
+
+class Options(argparse.ArgumentParser):
+    def __init__(self, prog=NAME, epilog=None):
+        usage = col.purple(sequana_prolog.format(**{"name": NAME}))
+        super(Options, self).__init__(usage=usage, prog=prog, description="",
+            epilog=epilog,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+        # add a new group of options to the parser
+        so = SlurmOptions()
+        so.add_options(self)
+
+        # add a snakemake group of options to the parser
+        so = SnakemakeOptions(working_directory=NAME)
+        so.add_options(self)
+
+        so = InputOptions()
+        so.add_options(self)
+
+        so = GeneralOptions()
+        so.add_options(self)
+
+        pipeline_group = self.add_argument_group("pipeline_general")
+        pipeline_group.add_argument("--aligner", dest="aligner",
+            choices=['bowtie1'], default='bowtie1',
+            help= "a mapper in bowtie")
+        pipeline_group.add_argument("--rRNA-feature",
+            default="rRNA",
+            help="""Feature name corresponding to the rRNA to be identified for QCs""")
+        pipeline_group.add_argument("--rRNA-file",
+            default=None,
+            help="""If you already have the rRNA file, just provide it""")
+        pipeline_group.add_argument("--genbank-file",
+            default=None,
+            help="""genbank. if provided, do not provide gff""")
+        pipeline_group.add_argument("--gff-file",
+            default=None,
+            help="""If provided, do not provide genbank""")
+        pipeline_group.add_argument("--reference-file",
+            default=None,
+            help="""The required referenceto fetch features into""")
+
+
+
+    def parse_args(self, *args):
+        args_list = list(*args)
+        if "--from-project" in args_list:
+            if len(args_list)>2:
+                msg = "WARNING [sequana]: With --from-project option, " + \
+                        "pipeline and data-related options will be ignored."
+                print(col.error(msg))
+            for action in self._actions:
+                if action.required is True:
+                    action.required = False
+        options = super(Options, self).parse_args(*args)
+        return options
+
+
+def main(args=None):
+
+    if args is None:
+        args = sys.argv
+
+    # whatever needs to be called by all pipeline before the options parsing
+    from sequana_pipetools.options import before_pipeline
+    before_pipeline(NAME)
+
+    # option parsing including common epilog
+    options = Options(NAME, epilog=sequana_epilog).parse_args(args[1:])
+
+
+    from sequana.pipelines_common import SequanaManager
+
+    # the real stuff is here
+    manager = SequanaManager(options, NAME)
+
+    # create the beginning of the command and the working directory
+    manager.setup()
+    from sequana import logger
+    logger.level = options.level
+
+    # fill the config file with input parameters
+    if options.from_project is None:
+        cfg = manager.config.config
+
+        # --------------------------------------------------------- general
+        cfg.general.aligner = options.aligner
+
+        cfg.general.rRNA_feature = options.rRNA_feature
+        if options.rRNA_file:
+            cfg.general.rRNA_file = os.path.abspath(options.rRNA_file)
+        if options.genbank_file:
+            cfg.general.genbank_file = os.path.abspath(options.genbank_file)
+        if options.gff_file:
+            cfg.general.gff_file = os.path.abspath(options.gff_file)
+        if options.reference_file:
+            cfg.general.reference_file = os.path.abspath(options.reference_file)
+
+        # ----------------------------------------------------  others
+        cfg.input_directory = os.path.abspath(options.input_directory)
+        cfg.input_pattern = options.input_pattern
+        cfg.input_readtag = options.input_readtag
+
+        # SANITY CHECKS
+        # -------------------------------------- do we find rRNA feature in the GFF ?
+        logger.info("checking your input GFF file and rRNA feature if provided")
+
+
+    # finalise the command and save it; copy the snakemake. update the config
+    # file and save it.
+    manager.teardown()
+
+if __name__ == "__main__":
+    main()
