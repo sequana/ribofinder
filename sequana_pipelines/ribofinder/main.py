9730dca9f7048bfcf3892090c153bd8523e4139c
Thomas Cokelaer <cokelaer@gmail.com>
2023-12-03 22:43:30 +0100

    Use new sequana_pipetools framework(click)

--- sequana_pipelines/ribofinder/main.py
+++ sequana_pipelines/ribofinder/main.py
@@ -12,139 +12,141 @@
 ##############################################################################
 import sys
 import os
-import argparse
 import subprocess
+import shutil
+from pathlib import Path
+
+import rich_click as click
+import click_completion
+
+click_completion.init()
 
 from sequana_pipetools.options import *
-from sequana_pipetools.options import before_pipeline
-from sequana_pipetools.misc import Colors
-from sequana_pipetools.info import sequana_epilog, sequana_prolog
 from sequana_pipetools import SequanaManager
-from sequana import logger
 
-col = Colors()
 
 NAME = "ribofinder"
 
-
-class Options(argparse.ArgumentParser):
-    def __init__(self, prog=NAME, epilog=None):
-        usage = col.purple(sequana_prolog.format(**{"name": NAME}))
-        super(Options, self).__init__(usage=usage, prog=prog, description="",
-            epilog=epilog,
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter
-        )
-        # add a new group of options to the parser
-        so = SlurmOptions()
-        so.add_options(self)
-
-        # add a snakemake group of options to the parser
-        so = SnakemakeOptions(working_directory=NAME)
-        so.add_options(self)
-
-        so = InputOptions()
-        so.add_options(self)
-
-        so = GeneralOptions()
-        so.add_options(self)
-
-        pipeline_group = self.add_argument_group("pipeline_general")
-        pipeline_group.add_argument("--aligner", dest="aligner",
-            choices=['bowtie1'], default='bowtie1',
-            help= "a mapper in bowtie")
-        pipeline_group.add_argument("--rRNA-feature",
-            default="rRNA",
-            help="""Feature name corresponding to the rRNA to be identified for QCs""")
-        pipeline_group.add_argument("--rRNA-file",
-            default=None,
-            help="""If you already have the rRNA file, just provide it""")
-        pipeline_group.add_argument("--genbank-file",
-            default=None,
-            help="""genbank. if provided, do not provide gff""")
-        pipeline_group.add_argument("--gff-file",
-            default=None,
-            help="""If provided, do not provide genbank""")
-        pipeline_group.add_argument("--reference-file",
-            default=None,
-            help="""The required referenceto fetch features into""")
+help = init_click(
+    NAME,
+    groups={
+        "Pipeline Specific": [
+            "--aligner",
+            "--genbank-file",
+            "--gff-file",
+            "--rRNA-feature",
+            "--rRNA-file",
+            "--reference-file",
+        ],
+    },
+)
 
 
-        self.add_argument("--run", default=False, action="store_true",
-            help="execute the pipeline directly")
-
-    def parse_args(self, *args):
-        args_list = list(*args)
-        if "--from-project" in args_list:
-            if len(args_list)>2:
-                msg = "WARNING [sequana]: With --from-project option, " + \
-                        "pipeline and data-related options will be ignored."
-                print(col.error(msg))
-            for action in self._actions:
-                if action.required is True:
-                    action.required = False
-        options = super(Options, self).parse_args(*args)
-        return options
-
-
-def main(args=None):
-
-    if args is None:
-        args = sys.argv
-
-    # whatever needs to be called by all pipeline before the options parsing
-    before_pipeline(NAME)
-
-    # option parsing including common epilog
-    options = Options(NAME, epilog=sequana_epilog).parse_args(args[1:])
-
+@click.command(context_settings=help)
+@include_options_from(ClickSnakemakeOptions, working_directory=NAME)
+@include_options_from(ClickSlurmOptions)
+@include_options_from(ClickInputOptions)
+@include_options_from(ClickGeneralOptions)
+@click.option(
+    "--aligner", "aligner", type=click.Choice(["bowtie1"]), default="bowtie1", help="the alignement tool (bowtie1)"
+)
+@click.option(
+    "--rRNA-feature",
+    "rRNA_feature",
+    default="rRNA",
+    type=click.STRING,
+    help="""Feature name corresponding to the rRNA to be identified for QCs""",
+)
+@click.option(
+    "--rRNA-file",
+    "rRNA_file",
+    default=None,
+    type=click.Path(file_okay=True, dir_okay=False),
+    help="""If you already have the rRNA file, just provide it""",
+)
+@click.option("--genbank-file", default=None, help="""genbank. if provided, do not provide gff""")
+@click.option("--gff-file", default=None, help="""If provided, do not provide genbank""")
+@click.option("--reference-file", default=None, help="""The required referenceto fetch features into""")
+def main(**options):
     # the real stuff is here
     manager = SequanaManager(options, NAME)
 
     # create the beginning of the command and the working directory
     manager.setup()
 
-    # fill the config file with input parameters
-    if options.from_project is None:
-        cfg = manager.config.config
+    # aliases
+    options = manager.options
+    cfg = manager.config.config
 
-        # --------------------------------------------------------- general
+    # fills input_data, input_directory, input_readtag
+    # needs an update of sequana_pipetools to exclude readtag
+    manager.fill_data_options()
+
+    # fill the config file with input parameters
+
+    # --------------------------------------------------------- general
+    def fill_aligner():
         cfg.general.aligner = options.aligner
 
+    def fill_rRNA_feature():
         cfg.general.rRNA_feature = options.rRNA_feature
+
+    def fill_rRNA_file():
         if options.rRNA_file:
             cfg.general.rRNA_file = os.path.abspath(options.rRNA_file)
+
+    def fill_genbank_file():
         if options.genbank_file:
             cfg.general.genbank_file = os.path.abspath(options.genbank_file)
+
+    def fill_gff_file():
         if options.gff_file:
             cfg.general.gff_file = os.path.abspath(options.gff_file)
+
+    def fill_reference_file():
         if options.reference_file:
             cfg.general.reference_file = os.path.abspath(options.reference_file)
 
+    if options["from_project"]:
+        if "--aligner" in sys.argv:
+            fill_aligner()
+        if "--rRNA-feature" in sys.argv:
+            fill_rRNA_feature()
+        if "--rRNA-file" in sys.argv:
+            fill_rRNA_file()
+        if "--genbank-file" in sys.argv:
+            fill_genbank_file()
+        if "--gff-file" in sys.argv:
+            fill_gff_file()
+        if "--reference-fle" in sys.argv:
+            fill_reference_file()
+
+    else:
+        fill_aligner()
+        fill_rRNA_feature()
+        fill_rRNA_file()
+        fill_genbank_file()
+        fill_gff_file()
+        fill_reference_file()
+
         if options.reference_file is None and options.rRNA_file is None:
-            logger.error("You must provide a rRNA file or a reference_file")
+            click.echo("You must provide a rRNA file or a reference_file", err=True)
             sys.exit(1)
 
         if options.reference_file:
-            logger.info("checking your input GFF file and rRNA feature if provided")
+            click.echo("checking your input GFF file and rRNA feature if provided")
             if options.genbank_file:
                 from sequana.genbank import GenBank
+
                 gbk = GenBank(options.genbank_file)
             if options.genbank_file is None and options.gff_file is None:
-                logger.error("Most probably you want to provide an annotation (genbank-file or gff-file)")
+                click.echo("Most probably you want to provide an annotation (genbank-file or gff-file)", err=True)
                 sys.exit(0)
 
-        # ----------------------------------------------------  others
-        cfg.input_pattern = options.input_pattern
-        cfg.input_directory = os.path.abspath(options.input_directory)
-        cfg.input_readtag = options.input_readtag
-
-        manager.exists(cfg.input_directory)
     # finalise the command and save it; copy the snakemake. update the config
     # file and save it.
     manager.teardown()
 
-    if options.run:
-        subprocess.Popen(["sh", '{}.sh'.format(NAME)], cwd=options.workdir)
 
 if __name__ == "__main__":
     main()
